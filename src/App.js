214bfa3b7cafd57804ac40b24f5f74657ee92a53
Zhanna Popenko <zpopenko7@gmail.com>
2021-11-14 13:14:30 +0200

    Сreated pages and added routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "./pages/News";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<News />}/>
+        <Route path="profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
